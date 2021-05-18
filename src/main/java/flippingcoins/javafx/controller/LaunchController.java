f8df6cc9e96323be87e5a4fa7d7cbb2cc6b12cab
Fuad Jamilov <fdjmlv666@gmail.com>
2021-05-18 22:34:59 +0200

    GameController is created

--- src/main/java/flippingcoins/javafx/controller/LaunchController.java
+++ src/main/java/flippingcoins/javafx/controller/LaunchController.java
@@ -1,4 +1,48 @@
 package flippingcoins.javafx.controller;
 
+import util.javafx.*;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
 public class LaunchController {
+
+    private FXMLLoader fxmlLoader = new FXMLLoader();
+
+    @FXML
+    private TextField player1NameTextField;
+
+    @FXML
+    private TextField player2NameTextField;
+
+    @FXML
+    private Label errorLabel;
+
+    public void startAction(ActionEvent actionEvent) throws IOException {
+        errorLabel.setText("");
+
+        if(player1NameTextField.getText().isEmpty() && player2NameTextField.getText().isEmpty()){
+            errorLabel.setText("Players, please, enter your names!!!");
+        }
+        else if(player1NameTextField.getText().isEmpty()){
+            errorLabel.setText("Player1, please, enter your name!!!");
+        }
+        else if(player2NameTextField.getText().isEmpty()){
+            errorLabel.setText("Player2, please, enter your name!!!");
+        }
+        else{
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            ControllerHelper.loadAndShowFXML(fxmlLoader,"/fxml/game.fxml",stage);
+        }
+    }
 }
