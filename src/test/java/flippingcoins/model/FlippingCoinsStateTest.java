74a26d1f50d77df4af496991c9e495c800df79d6
Fuad Jamilov <fdjmlv666@gmail.com>
2021-05-23 18:18:55 +0200

    FlippingCoinStateTest test methods are added

--- src/test/java/flippingcoins/model/FlippingCoinsStateTest.java
+++ src/test/java/flippingcoins/model/FlippingCoinsStateTest.java
@@ -2,15 +2,43 @@ package flippingcoins.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class FlippingCoinsStateTest {
+    FlippingCoinsState flippingCoinsState = new FlippingCoinsState();
+
+    @Test
+    void initializeCoins() {
+        flippingCoinsState.initializeCoins();
+
+        for (int i = 0; i < flippingCoinsState.coinState.size();i++){
+            assertEquals(Coin.HEAD,flippingCoinsState.coinState.get(i));
+        }
+    }
 
     @Test
     void isGameOver() {
+        List<Coin> coinsTrue =
+                Arrays.asList(Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL);
+        List<Coin> coinsFalse =
+                Arrays.asList(Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.TAIL,Coin.HEAD);
+        assertTrue(flippingCoinsState.isGameOver(coinsTrue));
+        assertFalse(flippingCoinsState.isGameOver(coinsFalse));
+    }
 
+    @Test
+    void modifyCoinState() {
+        flippingCoinsState.initializeCoins();
+        flippingCoinsState.coinState.set(1,Coin.TAIL);
+
+        flippingCoinsState.modifyCoinState(0);
+        flippingCoinsState.modifyCoinState(1);
+
+        assertEquals(Coin.TAIL,flippingCoinsState.coinState.get(0));
+        assertEquals(Coin.HEAD,flippingCoinsState.coinState.get(1));
     }
 }
